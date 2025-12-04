18f05e76e505dd0593c99f5de549a061714d825d
Oliver <oliversanchezalaminoscolegiolagomar.es>
2025-12-04 18:39:04 +0100

    Añadida variable de significado

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,5 +1,7 @@
 public class Ejercicio2 {
 public static void main(String[] args) {
-System.out.println("Clonado con éxito!");
+int numero = 42;
+System.out.println("El significado de la vida es: " + numero);
+
 }
 }
